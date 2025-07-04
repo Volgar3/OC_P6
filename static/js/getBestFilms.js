5f6ebf44c32b1058dfafbf14b98a1e23fe403a2f
Aurelien lebray <aurelien.lebray@gmail.com>
2025-07-04 14:51:34 +0200

    Lesson 20250704

--- static/js/getBestFilms.js
+++ static/js/getBestFilms.js
@@ -11,21 +11,18 @@ export function getBestFilms() {
             return response.json(); // or response.text(), depending on what your server returns
         })
         .then(data => {
-            fetch('http://localhost:8000/api/v1/titles/7822474') // change to your actual local URL and port
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json(); // or response.text(), depending on what your server returns
-                })
-                .then(data => {
-                    // displayToast(JSON.stringify(data, null, 2));
-                })
-                .catch(error => {
-                    displayToast('Error: ' + error);
-                });
+            console.log(data);
+            const container = document.getElementById('bestFilms');
+
+            const images = container.querySelectorAll('img');
+            let index = 0;
+            data.results.forEach(item => {
+                images[index].src = item.image_url;
+                index++;
+            });
         })
         .catch(error => {
+            console.log(error);
             displayToast(error.message);
         });
 }
