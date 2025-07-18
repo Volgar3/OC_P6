f351d9b94c41982657da680d548c5f8a2ee9ab26
Aurelien lebray <aurelien.lebray@gmail.com>
2025-07-18 14:14:00 +0200

    Lesson 20250718

--- static/js/index.js
+++ static/js/index.js
@@ -10,15 +10,11 @@ document.addEventListener("DOMContentLoaded", function () {
             const id = this.getAttribute('id').split('-')[1]
             console.log(id)
 
-<<<<<<< HEAD
-            // Changer le contenu de la modale dans le then de la requete , plutôt que ce qu'il y a en dessous
-=======
             const modalTitle = document.querySelector('.modal-title');
             if (modalTitle) {
                 modalTitle.textContent = `Titre : ${image.getAttribute('alt')}`;
             }
 
->>>>>>> 0fbcdeb8795b96d1c55969040a52d517d04bdf06
             const modalBody = document.querySelector('.modal-body'); // Get the first matching element
             if (modalBody) {
                 modalBody.textContent = `(ID : ${id})`;
