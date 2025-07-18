bde2bc5f08a93cf5defd518b5955000ee6c66edc
Aurelien lebray <aurelien.lebray@gmail.com>
2025-07-18 14:13:00 +0200

    Forgot to push

--- static/js/index.js
+++ static/js/index.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const id = this.getAttribute('id').split('-')[1]
             console.log(id)
 
+            // Changer le contenu de la modale dans le then de la requete , plutôt que ce qu'il y a en dessous
             const modalBody = document.querySelector('.modal-body'); // Get the first matching element
             if (modalBody) {
                 modalBody.textContent = altText;
