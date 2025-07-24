44145b393a08be060c6430ff7aa19d342ef04d33
Volgar3 <h.deles@gmail.com>
2025-07-24 12:42:20 +0200

    Avancement modal informations

--- static/js/index.js
+++ static/js/index.js
@@ -7,9 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
         img.addEventListener('click', function () {
             const id = this.getAttribute('id').split('-')[1]
             const modalTitle = document.querySelector('.modal-title');
+            const modalReleaseDate = document.querySelector('#modal-release');
+            //const ReleaseYear = data.date_published.split('-')[0] Trouver une solution
             const modalImage = document.querySelector('#modal-image');
             const modalDescription = document.querySelector('.modal-description > p');
             const modalGenre = document.querySelector('#modal-genre');
+            const modalDuration = document.querySelector('#modal-duration');
+            const modalDirector = document.querySelector('#modal-director');
+            const modalBoxOffice = document.querySelector('#modal-boxOffice'); // Trouver ce qu'il y a apres "data."
+            const modalIMBD = document.querySelector('#modal-IMDB');
+            const modalActors = document.querySelector('#modal-actors');
 
             fetch(`http://localhost:8000/api/v1/titles/${id}`).then(
                 response => {
@@ -22,6 +29,14 @@ document.addEventListener("DOMContentLoaded", function () {
                     modalImage.src = data.image_url;
                     modalDescription.textContent = data.long_description;
                     modalGenre.innerHTML = `<strong>Genres :</strong> ${data.genres.join(", ")}`;
+                    modalDuration.innerHTML = `<strong>Durée :</strong> ${data.duration} minutes`;
+                    modalDirector.innerHTML = `<strong>Réalisateur :</strong> ${data.directors}`;
+                    // A trouver modalBoxOffice.innerHTML = `<strong>Recettes au box-office :</strong>${data.}`;
+                    modalIMBD.innerHTML = `<strong>Score IMDB :</strong> ${data.avg_vote} /10`;
+                    modalActors.innerHTML = `<u>Acteurs et actrices :</u>  ${data.actors}`;
+                    modalReleaseDate.innerHTML =`<u>Sortie en :</u>  ${ReleaseYear}`; // Trouver une solution
+
+
                 }
             );
         });
